f2129128693df6445e5caf95b37f4c9bdf8f6558
Dmitry Jemerov <intelliyole@gmail.com>
2021-12-20 11:05:59 +0100

    day 20

---
+++ day20/day20.py
@@ -0,0 +1,31 @@
+lines = open("day20input.txt").readlines()
+algorithm = lines[0].strip()
+
+def safe_at(grid, x, y, background):
+    return background if y < 0 or y >= len(grid) or x < 0 or x >= len(grid[y]) else grid[y][x]
+
+def kernel(grid, x, y, background):
+    result = 0
+    for dy in range(-1, 2):
+        for dx in range(-1, 2):
+            pixel = 1 if safe_at(grid, x + dx, y + dy, background) == '#' else 0
+            result = result << 1 | pixel
+    return result
+
+def enhance_step(grid, background):
+    result = []
+    for y in range(-1, len(grid)+1):
+        line = ''
+        for x in range(-1, len(grid[0])+1):
+            line += algorithm[kernel(grid, x, y, background)]
+        result.append(line)
+    print('\n'.join(result) + '\n')
+    return result
+
+grid = [line.strip() for line in lines[2:]]
+print('\n'.join(grid) + '\n')
+grid = enhance_step(enhance_step(grid, '.'), algorithm[0])
+print(len([c for line in grid for c in line if c != '.']))
+
+
+
